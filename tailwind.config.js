9ee9729e20b8555cfb18090cee89a6768a3cc2c8
Kasper Nikolajsen Dahl <kasp103y@stud.kea.dk>
2022-05-01 14:11:00 +0200

    ting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: false,
+  },
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("daisyui"),
+  ],
 };
